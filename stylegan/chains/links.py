c1bcc81fbdbc34f579d4294707842551199a0885
curegit <37978051+curegit@users.noreply.github.com>
2020-07-10 15:35:22 +0900

    Update links

---
+++ stylegan/chains/links.py
@@ -0,0 +1,36 @@
+from math import sqrt
+from chainer import Chain, Link
+from chainer.links import Linear, Convolution2D
+from chainer.functions import leaky_relu
+from chainer.initializers import Normal
+
+class EqualizedLinear(Chain):
+
+	def __init__(self, in_size, out_size, initial_bias=None, gain=sqrt(2)):
+		super().__init__()
+		self.c = gain * sqrt(1 / in_size)
+		with self.init_scope():
+			self.linear = Linear(in_size, out_size, initialW=Normal(1.0), initial_bias=initial_bias)
+
+	def __call__(self, x):
+		return self.linear(self.c * x)
+
+class EqualizedConvolution2D(Chain):
+
+	def __init__(self, in_channels, out_channels, ksize=None, stride=1, pad=0, nobias=False, initial_bias=None, gain=sqrt(2)):
+		super().__init__()
+		self.c = gain * sqrt(1 / (in_channels * ksize ** 2))
+		with self.init_scope():
+			self.conv = Convolution2D(in_channels, out_channels, ksize, stride, pad, nobias=nobias, initialW=Normal(1.0), initial_bias=initial_bias)
+
+	def __call__(self, x):
+		return self.conv(self.c * x)
+
+class LeakyRelu(Link):
+
+	def __init__(self, a):
+		super().__init__()
+		self.a = a
+
+	def __call__(self, x):
+		return leaky_relu(x, self.a)
