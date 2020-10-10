0c12b34b15f3c1536d13915834f78a4332fcfbd5
curegit <37978051+curegit@users.noreply.github.com>
2020-10-10 21:24:13 +0900

    Update filesys util

--- utilities/filesys.py
+++ utilities/filesys.py
@@ -7,7 +7,7 @@ def mkdirs(dirpath):
 def filepath(dirpath, filename, fileext):
 	return os.path.normpath(os.path.join(dirpath, filename) + os.extsep + fileext)
 
-def altfilepath(filepath, suffix="+"):
+def alt_filepath(filepath, suffix="+"):
 	while os.path.lexists(filepath):
 		root, ext = os.path.splitext(filepath)
 		head, tail = os.path.split(root)
