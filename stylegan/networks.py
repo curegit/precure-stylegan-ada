13b688d65b7c1f73ce77ab672e522b0926cc4906
curegit <37978051+curegit@users.noreply.github.com>
2020-12-09 12:33:49 +0900

    Update net

--- stylegan/networks.py
+++ stylegan/networks.py
@@ -1,29 +1,56 @@
-from chainer import Chain, ChainList, Sequential
+from numpy import load, save
+from chainer import Variable, Chain, ChainList, Sequential
+from chainer.backend import CpuDevice
 from chainer.functions import sqrt, mean
+from chainer.serializers import load_hdf5, save_hdf5
 from stylegan.links.common import GaussianDistribution, EqualizedLinear, LeakyRelu
 from stylegan.links.generator import InitialSkipArchitecture, SkipArchitecture
 from stylegan.links.discriminator import FromRGB, ResidualBlock, OutputBlock
 
-class Mapper(Chain):
+class Network(Chain):
 
-	def __init__(self, latent_size, depth):
+	def load_model(self, filepath):
+		device = self.device
+		self.to_device(CpuDevice())
+		load_hdf5(filepath, self)
+		return self.to_device(device)
+
+	def save_model(self, filepath):
+		device = self.device
+		self.to_device(CpuDevice())
+		save_hdf5(filepath, self)
+		return self.to_device(device)
+
+	def load_variable(self, filepath):
+		x = Variable(load(filepath))
+		x.to_device(self.device)
+		return x
+
+	def save_variable(self, filepath, x):
+		x.to_device(CpuDevice())
+		save(filepath, x.array)
+		x.to_device(self.device)
+		return x
+
+class Mapper(Network):
+
+	def __init__(self, size, depth):
 		super().__init__()
 		with self.init_scope():
-			self.mlp = Sequential(EqualizedLinear(latent_size, latent_size), LeakyRelu()).repeat(depth)
+			self.mlp = Sequential(EqualizedLinear(size, size), LeakyRelu()).repeat(depth)
 
 	def __call__(self, z):
-		norm_z = z / sqrt(mean(z ** 2, axis=1, keepdims=True) + 1e-8)
-		return self.mlp(norm_z)
+		return self.mlp(z / sqrt(mean(z ** 2, axis=1, keepdims=True) + 1e-8))
 
-class Synthesizer(Chain):
+class Synthesizer(Network):
 
-	def __init__(self, latent_size, levels=7, first_channels=512, last_channels=16, double_last=True):
+	def __init__(self, size, levels=7, first_channels=512, last_channels=16, double_last=True):
 		super().__init__()
 		in_channels = [first_channels if i == 1 else min(first_channels, last_channels * 2 ** (levels - i + 1)) for i in range(1, levels + 1)]
 		out_channels = [last_channels * (2 if double_last else 1) if i == levels else min(first_channels, last_channels * 2 ** (levels - i)) for i in range(1, levels + 1)]
-		skips = [SkipArchitecture(latent_size, i, o) for i, o in zip(in_channels[1:], out_channels[1:])]
+		skips = [SkipArchitecture(size, i, o) for i, o in zip(in_channels[1:], out_channels[1:])]
 		with self.init_scope():
-			self.init = InitialSkipArchitecture(latent_size, in_channels[0], out_channels[0])
+			self.init = InitialSkipArchitecture(size, in_channels[0], out_channels[0])
 			self.skips = ChainList(*skips)
 
 	def __call__(self, ws):
@@ -32,7 +59,7 @@ class Synthesizer(Chain):
 			h, rgb = s(h, rgb, w)
 		return rgb
 
-class Generator(Chain):
+class Generator(Network):
 
 	def __init__(self, latent_size=512, depth=8, levels=7, first_channels=512, last_channels=16, double_last=True):
 		super().__init__()
@@ -50,7 +77,7 @@ class Generator(Chain):
 	def generate_latents(self, batch):
 		return self.sampler((batch, self.latent_size))
 
-class Discriminator(Chain):
+class Discriminator(Network):
 
 	def __init__(self, levels=7, first_channels=16, last_channels=512):
 		super().__init__()
