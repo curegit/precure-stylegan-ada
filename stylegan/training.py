e4aedf298c78f4b89a8646ef3ae207cb25d49480
curegit <37978051+curegit@users.noreply.github.com>
2020-12-17 11:16:19 +0900

    Update training

--- stylegan/training.py
+++ stylegan/training.py
@@ -1,18 +1,15 @@
-from chainer.training import Trainer, extensions, make_extension
-from chainer.iterators import MultiprocessIterator
-from random import random
-from chainer import grad
-from chainer.reporter import report
-from chainer.training import Updater, Trainer
-from chainer.functions import sum, batch_l2_norm_squared, softplus
-
-from chainer.iterators import SerialIterator
-
-from utilities.stdio import eprint
-
+import random
+import numpy as np
 from h5py import File as HDF5File
-
+from chainer import grad, Variable
+from chainer.reporter import report
+from chainer.training import StandardUpdater, Trainer
+from chainer.training.extensions import PrintReport, LogReport, PlotReport, ProgressBar
+from chainer.functions import sum, batch_l2_norm_squared, softplus
 from chainer.serializers import HDF5Serializer, HDF5Deserializer
+from utilities.iter import range_batch
+from utilities.image import save_image
+from utilities.filesys import build_filepath
 
 class OptimizerSet():
 
@@ -33,163 +30,114 @@ class OptimizerSet():
 	def update_discriminator(self):
 		self.discriminator_optimizer.update()
 
-	def load_state(self, filepath):
+	def load_states(self, filepath):
 		with HDF5File(filepath, "r") as hdf5:
 			HDF5Deserializer(hdf5["mapper"]).load(self.mapper_optimizer)
 			HDF5Deserializer(hdf5["synthesizer"]).load(self.synthesizer_optimizer)
 			HDF5Deserializer(hdf5["discriminator"]).load(self.discriminator_optimizer)
 
-	def save_state(self, filepath):
+	def save_states(self, filepath):
 		with HDF5File(filepath, "w") as hdf5:
 			HDF5Serializer(hdf5.create_group("mapper")).save(self.mapper_optimizer)
 			HDF5Serializer(hdf5.create_group("synthesizer")).save(self.synthesizer_optimizer)
 			HDF5Serializer(hdf5.create_group("discriminator")).save(self.discriminator_optimizer)
 
-class CustomUpdater(Updater):
+class CustomUpdater(StandardUpdater):
 
-	def __init__(self, generator, discriminator, iterator, optimizers, mixing=0.5, gamma=10, lsgan=False):
-		super().__init__()
+	def __init__(self, generator, discriminator, iterator, optimizers, mixing_rate=0.5, gamma=10, lsgan=False):
+		super().__init__(iterator, optimizers)
 		self.generator = generator
 		self.discriminator = discriminator
 		self.iterator = iterator
 		self.optimizers = optimizers
-		self.mixing = mixing
+		self.mixing_rate = mixing_rate
 		self.gamma = gamma
 		self.lsgan = lsgan
 
 	def update_core(self):
-		batch = self.iterator.next()
-		batchsize = len(batch)
+		self.update_discriminator()
+		self.update_generator()
 
-		# Train discriminator
-		x_real = self.discriminator.wrap_array(batch)
+	def update_generator(self):
+		self.generator.cleargrads()
+		z = self.generator.generate_latents(self.iterator.batch_size)
+		mix = self.generator.generate_latents(self.iterator.batch_size) if random.random() < self.mixing_rate else None
+		x_fake = self.generator(z, random_mix=mix)
+		y_fake = self.discriminator(x_fake)
+		loss = (sum((y_fake - 1) ** 2) / 2 if self.lsgan else sum(softplus(-y_fake))) / self.iterator.batch_size
+		loss.backward()
+		self.optimizers.update_generator()
+		report({"loss (G)": loss})
+
+	def update_discriminator(self):
+		self.discriminator.cleargrads()
+		x_real = Variable(self.iterator.next())
+		x_real.to_device(self.discriminator.device)
 		y_real = self.discriminator(x_real)
 		gradient = grad([y_real], [x_real], enable_double_backprop=True)[0]
-		gradient_norm = sum(batch_l2_norm_squared(gradient)) / batchsize
-		loss_grad = self.gamma * gradient_norm / 2
-		z = self.generator.generate_latent(batchsize)
-		#mix_z = self.generator.generate_latent(batchsize) if self.mixing > random() else None
-		x_fake = self.generator(z) # TODO
+		gradient_norm = sum(batch_l2_norm_squared(gradient)) / self.iterator.batch_size
+		penalty = self.gamma * gradient_norm / 2
+		z = self.generator.generate_latents(self.iterator.batch_size)
+		mix = self.generator.generate_latents(self.iterator.batch_size) if random.random() < self.mixing_rate else None
+		x_fake = self.generator(z, random_mix=mix)
 		y_fake = self.discriminator(x_fake)
-		loss_dis = ((sum((y_real - 1) ** 2) + sum(y_fake ** 2)) / 2 if self.lsgan else (sum(softplus(-y_real)) + sum(softplus(y_fake)))) / batchsize
-		loss_dis += loss_grad
 		x_fake.unchain_backward()
-		self.discriminator.cleargrads()
-		loss_dis.backward()
+		loss = ((sum((y_real - 1) ** 2) + sum(y_fake ** 2)) / 2 if self.lsgan else (sum(softplus(-y_real)) + sum(softplus(y_fake)))) / self.iterator.batch_size
+		loss += penalty
+		loss.backward()
 		self.optimizers.update_discriminator()
-		#self.discriminator_optimizer.update()
-
-		# Train generator
-		z = self.generator.generate_latent(batchsize)
-		#mix_z = self.generator.generate_latent(batchsize) if self.mixing > random() else None
-		x_fake = self.generator(z) # TODO
-		y_fake = self.discriminator(x_fake)
-		loss_gen = (sum((y_fake - 1) ** 2) / 2 if self.lsgan else sum(softplus(-y_fake))) / batchsize
-		self.generator.cleargrads()
-		loss_gen.backward()
-		self.optimizers.update_generator()
-		#self.mapper_optimizer.update()
-		#self.generator_optimizer.update()
-
-		#report({"alpha": self.alpha})
-		report({"loss (gen)": loss_gen})
-		report({"loss (dis)": loss_dis})
-		report({"loss (grad)": loss_grad})
-		#self.alpha = min(1.0, self.alpha + self.delta)
-
-"""
-# Define extension to output images in progress
-def save_middle_images(generator, stage, directory, number, batch, mix, force=True, save_latent=True):
-	@make_extension()
-	def func(trainer):
-		c = 0
-		mixing = mix > random()
-		while c < number:
-			n = min(number - c, batch)
-			z = generator.generate_latent(n)
-			mix_z = generator.generate_latent(n) if mixing else None
-			y = generator(z, stage, trainer.updater.alpha, mix_z)
-			z.to_cpu()
-			y.to_cpu()
-			for i in range(n):
-				path = filepath(directory, f"{stage}_{trainer.updater.iteration}_{trainer.updater.alpha:.3f}_{c + i + 1}", "png")
-				path = path if force else altfilepath(path)
-				save_image(y.array[i], path)
-				if save_latent:
-					path = filepath(directory, f"{stage}_{trainer.updater.iteration}_{trainer.updater.alpha:.3f}_{c + i + 1}", "npy")
-					path = path if force else altfilepath(path)
-					save_array(z.array[i], path)
-			c += n
-	return func
-
-# Define extension to save models in progress
-def save_middle_models(generator, discriminator, stage, directory, device, force=True):
-	@make_extension()
-	def func(trainer):
-		generator.to_cpu()
-		discriminator.to_cpu()
-		path = filepath(directory, f"gen_{stage}_{trainer.updater.iteration}", "hdf5")
-		path = path if force else altfilepath(path)
-		serializers.save_hdf5(path, generator)
-		path = filepath(directory, f"dis_{stage}_{trainer.updater.iteration}", "hdf5")
-		path = path if force else altfilepath(path)
-		serializers.save_hdf5(path, discriminator)
-		if device >= 0:
-			generator.to_gpu(device)
-			discriminator.to_gpu(device)
-	return func
-
-# Define extension to save optimizers in progress
-def save_middle_optimizers(mapper_optimizer, generator_optimizer, discriminator_optimizer, stage, directory, force=True):
-	@make_extension()
-	def func(trainer):
-		path = filepath(directory, f"mopt_{stage}_{trainer.updater.iteration}", "hdf5")
-		path = path if force else altfilepath(path)
-		serializers.save_hdf5(path, mapper_optimizer)
-		path = filepath(directory, f"gopt_{stage}_{trainer.updater.iteration}", "hdf5")
-		path = path if force else altfilepath(path)
-		serializers.save_hdf5(path, generator_optimizer)
-		path = filepath(directory, f"dopt_{stage}_{trainer.updater.iteration}", "hdf5")
-		path = path if force else altfilepath(path)
-		serializers.save_hdf5(path, discriminator_optimizer)
-	return func
-"""
+		report({"loss (D)": loss, "penalty (D)": penalty})
 
 class CustomTrainer(Trainer):
 
-	def __init__(self, updater, epoch, dest):
+	def __init__(self, updater, epoch, dest, overwrite=False):
 		super().__init__(updater, (epoch, "epoch"), dest)
-		self.dest = dest
-		#i = updater.iterator.batch_size
-		self.generator = updater.generator
-		self.discriminator = updater.discriminator
-		self.number = 100
+		self.overwrite = overwrite
+		self.number = 16
 
-"""
-	@make_extension()
+	def enable_reports(self, interval):
+		entries = ["epoch", "iteration", "loss (G)", "loss (D)", "penalty (D)"]
+		from os.path import basename
+		filename = basename(build_filepath(self.out, "report", "log", self.overwrite))
+		log_reporter = LogReport(trigger=(interval, "iteration"), filename=filename)
+		self.extend(log_reporter)
+		self.extend(PrintReport(entries, log_reporter))
+		filename = basename(build_filepath(self.out, "plot", "png", self.overwrite))
+		PlotReport(entries[2:], "iteration", trigger=(interval, "iteration"), filename=filename)
+
+	def enable_progress_bar(self, interval):
+		self.extend(ProgressBar(update_interval=interval))
+
+	def hook_state_save(self, interval):
+		self.extend(CustomTrainer.save_model_states, trigger=(interval, "iteration"))
+		self.extend(CustomTrainer.save_optimizer_states, trigger=(interval, "iteration"))
+
+	def hook_image_generation(self, interval, number):
+		self.number = number
+		self.extend(CustomTrainer.save_middle_images, trigger=(interval, "iteration"))
+
+	@staticmethod
+	def save_model_states(trainer):
+		iteration = trainer.updater.iteration
+		filepath = build_filepath(trainer.out, f"gen_{iteration}", "hdf5", trainer.overwrite)
+		trainer.updater.generator.save_state(filepath)
+		filepath = build_filepath(trainer.out, f"dis_{iteration}", "hdf5", trainer.overwrite)
+		trainer.updater.discriminator.save_state(filepath)
+
+	@staticmethod
+	def save_optimizer_states(trainer):
+		iteration = trainer.updater.iteration
+		filepath = build_filepath(trainer.out, f"opt_{iteration}", "hdf5", trainer.overwrite)
+		trainer.updater.optimizers.save_states(filepath)
+
+	@staticmethod
 	def save_middle_images(trainer):
-		trainer.generator
-		number = self.number or
-		for i in range(0, number, batch):
-			n = min(number - i, batch)
-
-
-		c = 0
-		mixing = mix > random()
-		while c < number:
-			n = min(number - c, batch)
-			z = generator.generate_latent(n)
-			mix_z = generator.generate_latent(n) if mixing else None
-			y = generator(z, stage, trainer.updater.alpha, mix_z)
+		for i, n in range_batch(trainer.number, trainer.updater.iterator.batch_size):
+			z = trainer.updater.generator.generate_latents(n)
+			y = trainer.updater.generator(z)
 			z.to_cpu()
 			y.to_cpu()
-			for i in range(n):
-				path = filepath(directory, f"{stage}_{trainer.updater.iteration}_{trainer.updater.alpha:.3f}_{c + i + 1}", "png")
-				path = path if force else altfilepath(path)
-				save_image(y.array[i], path)
-				if save_latent:
-					path = filepath(directory, f"{stage}_{trainer.updater.iteration}_{trainer.updater.alpha:.3f}_{c + i + 1}", "npy")
-					path = path if force else altfilepath(path)
-					save_array(z.array[i], path)
-			c += n
-"""
+			for j in range(n):
+				filename = f"{trainer.updater.iteration}{i + j + 1}"
+				np.save(build_filepath(trainer.out, filename, "npy", trainer.overwrite), z.array[j])
+				save_image(y.array[j], build_filepath(trainer.out, filename, "png", trainer.overwrite))
