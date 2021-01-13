522b53bae60a7db94e47638cf0f5e16d239137df
curegit <37978051+curegit@users.noreply.github.com>
2021-01-13 15:30:13 +0900

    Employ mod for TRGB

--- stylegan/links/generator.py
+++ stylegan/links/generator.py
@@ -4,16 +4,6 @@ from chainer.functions import sqrt, sum, convolution_2d, resize_images, broadcas
 from chainer.initializers import Zero, One, Normal
 from stylegan.links.common import GaussianDistribution, EqualizedLinear, EqualizedConvolution2D, LeakyRelu
 
-class ToRGB(Chain):
-
-	def __init__(self, in_channels):
-		super().__init__()
-		with self.init_scope():
-			self.c = EqualizedConvolution2D(in_channels, 3, ksize=1, stride=1, pad=0, gain=1)
-
-	def __call__(self, x):
-		return self.c(x)
-
 class Upsampler(Link):
 
 	def __init__(self):
@@ -35,8 +25,9 @@ class StyleAffineTransform(Chain):
 
 class WeightDemodulatedConvolution2D(Link):
 
-	def __init__(self, in_channels, out_channels):
+	def __init__(self, in_channels, out_channels, demod=True):
 		super().__init__()
+		self.demod = demod
 		with self.init_scope():
 			self.w = Parameter(shape=(out_channels, in_channels, 3, 3), initializer=Normal(1.0))
 			self.b = Parameter(shape=out_channels, initializer=Zero())
@@ -45,7 +36,7 @@ class WeightDemodulatedConvolution2D(Link):
 		out_channels = self.b.shape[0]
 		batch, in_channels, height, width = x.shape
 		w = self.w * y.reshape((batch, 1, in_channels, 1, 1))
-		demod_w = w / sqrt(sum(w ** 2, axis=(2, 3, 4), keepdims=True) + 1e-8)
+		demod_w = w / sqrt(sum(w ** 2, axis=(2, 3, 4), keepdims=True) + 1e-8) if self.demod else w
 		group_w = demod_w.reshape((batch * out_channels, in_channels, 3, 3))
 		group_x = x.reshape((1, batch * in_channels, height, width))
 		h = convolution_2d(group_x, group_w, stride=1, pad=1, groups=batch)
@@ -64,6 +55,16 @@ class NoiseAdder(Chain):
 		n = broadcast_to(self.g((b, 1, h, w)), x.shape)
 		return x + self.s(n)
 
+class ToRGB(Chain):
+
+	def __init__(self, in_channels):
+		super().__init__()
+		with self.init_scope():
+			self.w = WeightDemodulatedConvolution2D(in_channels, 3, False)
+
+	def __call__(self, x, y):
+		return self.w(x, y)
+
 class InitialSkipArchitecture(Chain):
 
 	def __init__(self, size, in_channels, out_channels):
@@ -74,6 +75,7 @@ class InitialSkipArchitecture(Chain):
 			self.w1 = WeightDemodulatedConvolution2D(in_channels, out_channels)
 			self.n1 = NoiseAdder(out_channels)
 			self.a1 = LeakyRelu()
+			self.s3 = StyleAffineTransform(size, out_channels)
 			self.trgb = ToRGB(out_channels)
 
 	def __call__(self, w):
@@ -83,7 +85,7 @@ class InitialSkipArchitecture(Chain):
 		h3 = self.w1(h2, self.s1(w))
 		h4 = self.n1(h3)
 		h5 = self.a1(h4)
-		return h5, self.trgb(h5)
+		return h5, self.trgb(h5, self.s3(w))
 
 class SkipArchitecture(Chain):
 
@@ -99,6 +101,7 @@ class SkipArchitecture(Chain):
 			self.w2 = WeightDemodulatedConvolution2D(out_channels, out_channels)
 			self.n2 = NoiseAdder(out_channels)
 			self.a2 = LeakyRelu()
+			self.s3 = StyleAffineTransform(size, out_channels)
 			self.trgb = ToRGB(out_channels)
 			self.skip = Upsampler()
 
@@ -110,4 +113,4 @@ class SkipArchitecture(Chain):
 		h5 = self.w2(h4, self.s2(w))
 		h6 = self.n2(h5)
 		h7 = self.a2(h6)
-		return h7, self.skip(y) + self.trgb(h7)
+		return h7, self.skip(y) + self.trgb(h7, self.s3(w))
