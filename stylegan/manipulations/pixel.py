47fc223d95e72ac03120535a4bd7c59e9b737a1a
curegit <37978051+curegit@users.noreply.github.com>
2021-04-18 00:11:51 +0900

    Fix

--- stylegan/manipulations/pixel.py
+++ stylegan/manipulations/pixel.py
@@ -6,8 +6,8 @@ class Manipulation(Link):
 	def random_where(self, p, x, y, axis=0):
 		axes = axis if axis is tuple else tuple([axis])
 		shape = tuple([n if i in axes else 1 for i, n in enumerate(x.shape)])
-		condition = Variable(self.xp.random.uniform(size=shape).astype(self.xp.float32)) < p
-		return where(broadcast_to(condition, x.shape), x, y)
+		condition = self.xp.random.uniform(size=shape).astype(self.xp.float32) < p
+		return where(broadcast_to(Variable(condition), x.shape), x, y)
 
 class Mirror(Manipulation):
 
