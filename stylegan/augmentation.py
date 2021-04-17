49bd7b6c9edc260e5646dbe5c1f06a9c4f6be113
curegit <37978051+curegit@users.noreply.github.com>
2021-04-17 23:36:33 +0900

    Add a few augumentations

--- stylegan/augmentation.py
+++ stylegan/augmentation.py
@@ -1,13 +1,17 @@
+from chainer import Chain, ChainList
+from stylegan.manipulations.pixel import Mirror, Rotation
 
-class AugmentationPipeline():
+class AugmentationPipeline(Chain):
 
-	def __init__(self, initial_probability=0.5):
+	def __init__(self, initial_probability=0.0):
 		super().__init__()
-		self.p = initial_probability
-		self.manipulations = []
+		self.probability = initial_probability
+		with self.init_scope():
+			self.manipulations = ChainList(*[
+				Mirror(),
+				Rotation()])
 
 	def __call__(self, x):
-		# pseudo
-		for m in manipulations:
-			for i in x:
-				m(i) if random < p else i
+		for f in self.manipulations:
+			x = f(x, self.probability)
+		return x
---
+++ stylegan/manipulations/pixel.py
@@ -0,0 +1,26 @@
+from chainer import Variable, Link
+from chainer.functions import flip, where, broadcast_to
+
+class Manipulation(Link):
+
+	def random_where(self, p, x, y, axis=0):
+		axes = axis if axis is tuple else tuple([axis])
+		shape = tuple([n if i in axes else 1 for i, n in enumerate(x.shape)])
+		condition = self.xp.random.uniform(size=shape).astype(self.xp.float32) < p
+		return where(broadcast_to(condition, x.shape), x, y)
+
+class Mirror(Manipulation):
+
+	def __call__(self, x, p):
+		mirrored = self.random_where(0.5, x, flip(x, axis=3))
+		return self.random_where(p, mirrored, x)
+
+class Rotation(Manipulation):
+
+	def __call__(self, x, p):
+		transposed = x.transpose(0, 1, 3, 2)
+		rot90 = flip(transposed, axis=2)
+		rot180 = flip(flip(x, axis=2), axis=3)
+		rot270 = flip(transposed, axis=3)
+		rotated = self.random_where(0.5, self.random_where(0.5, x, rot90), self.random_where(0.5, rot180, rot270))
+		return self.random_where(p, rotated, x)
