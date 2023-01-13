e8cbc92b40c50745d0b290a5bd696e2b1f5c3065
curegit <37978051+curegit@users.noreply.github.com>
2023-01-13 11:21:13 +0900

    Update mix.py

--- mix.py
+++ mix.py
@@ -13,6 +13,34 @@ from utilities.stdio import eprint
 from utilities.filesys import mkdirs, build_filepath
 from utilities.chainer import to_variable, config_valid
 
+def justify(xs, length, align_end=False, fill=...):
+	ys = []
+	ixs = enumerate(xs)
+	j, x = next(ixs)
+	ys.append(x)
+	for i in range(1, length):
+		if align_end:
+			k = i * (len(xs) - 1) // (length - 1)
+		else:
+			k = i * len(xs) // length
+		if k >= j + 1:
+			j, x = next(ixs)
+			ys.append(x)
+		else:
+			ys.append(fill)
+	return ys
+
+def slide_ellipsis(xs):
+	ys = []
+	_, lw = first(xs, lambda x: x is not ...)
+	for w in xs:
+		if w is ...:
+			ys.append(lw)
+		else:
+			ys.append(w)
+			lw = w
+	return ys
+
 def lerp_ellipsis(xs):
 	ys = []
 	left, lw = None, None
@@ -35,23 +63,6 @@ def lerp_ellipsis(xs):
 			right, (_, rw) = first(enumerate(xs), lambda ix: ix[0] > i and ix[1] is not ..., default=(-1, None))
 	return ys
 
-def justify(xs, length, align_end=False, fill=...):
-	ys = []
-	ixs = enumerate(xs)
-	j, x = next(ixs)
-	ys.append(x)
-	for i in range(1, length):
-		if align_end:
-			k = i * (len(xs) - 1) // (length - 1)
-		else:
-			k = i * len(xs) // length
-		if k >= j + 1:
-			j, x = next(ixs)
-			ys.append(x)
-		else:
-			ys.append(fill)
-	return ys
-
 def main(args):
 	config_valid()
 	print("Loading a model...")
@@ -60,13 +71,7 @@ def main(args):
 	ws = []
 	for s in args.style:
 		if (s == "..."):
-			if args.lerp:
-				ws.append(...)
-			elif (ws):
-				ws.append(ws[-1])
-			else:
-				eprint("You must supply a 1st level style!")
-				raise RuntimeError("Input error")
+			ws.append(...)
 		else:
 			ws.append(to_variable(np.load(s), device=args.device))
 	if all(w is ... for w in ws):
@@ -76,9 +81,23 @@ def main(args):
 		eprint("Too many styles!")
 		raise RuntimeError("Input error")
 	if (len(ws) != generator.levels):
-		ws += [ws[-1]] * (generator.levels - len(ws))
+		if args.justify:
+			ws = justify(ws, generator.levels, align_end=args.lerp)
+		else:
+			ws += [...] * (generator.levels - len(ws))
+	styles = []
+	count = 0
+	for w in ws:
+		if w is ...:
+			styles.append("...")
+		else:
+			count += 1
+			styles.append(f"$style{count}")
+	print("mix(" + ", ".join(styles) + ")")
 	if args.lerp:
 		ws = lerp_ellipsis(ws)
+	else:
+		ws = slide_ellipsis(ws)
 	mkdirs(args.dest)
 	with chainer_like_tqdm(desc="generation", total=args.number) as bar:
 		for i, n in range_batch(args.number, args.batch):
@@ -93,8 +112,8 @@ def parse_args():
 	parser = CustomArgumentParser("Mix style vectors to compose feature-mixed images")
 	parser.require_generator().add_output_args("mixtures")
 	parser.add_argument("style", metavar="STYLE_FILE", nargs="+", help="input style NPY file for each level, specify '...' to use the previous level's one (you can omit the tailing '...')")
-	parser.add_argument("-a", "--auto", action="store_true", help="")
-	parser.add_argument("-l", "--lerp", action="store_true", help="")
+	parser.add_argument("-j", "--justify", action="store_true", help="justify input style arguments evenly automatically")
+	parser.add_argument("-l", "--lerp", action="store_true", help="use linear interpolation to resolve '...' instead of repeating")
 	parser.add_argument("-n", "--number", metavar="N", type=uint, default=10, help="the number of images to generate")
 	return parser.add_evaluation_args().parse_args()
 
